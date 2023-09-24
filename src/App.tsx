9d1776af1b95dd141c6c02c40aef7bcabb95bb1e
Waheed <a.waheed3738@gmail.com>
2023-09-24 16:00:35 +0500

    add data fetching and reducer to manage to manage state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,57 @@
 import './App.css'
 import Header from "./components/Header.tsx";
 import Main from "./components/Main.tsx";
+import {useEffect, useReducer} from "react";
+import axios, {AxiosError} from "axios";
+
+enum Status {
+    loading = 'loading',
+    ready = 'ready',
+    error = 'error'
+}
+
+interface ReducerState {
+    data: []
+    status: Status
+}
+
+type actionType = { type: 'fetch', payload: [] } | { type: 'error' }
+const reducer = (state: ReducerState, action: actionType) => {
+    switch (action.type) {
+        case 'fetch' :
+            return {...state, data: action.payload, status: Status.ready}
+        case "error":
+            return {...state, status: Status.error}
+        default :
+            return state
+    }
+
+}
+
+const initialState: ReducerState = {
+    data: [],
+    status: Status.loading
+}
 
 function App() {
-  return (
-      <div className={'bg-gray-900'}>
-          <Header/>
-          <Main>asd</Main>
-      </div>
-  )}
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+    useEffect(() => {
+        axios.get('http://localhost:8000/questions').then(res => {
+            dispatch({type: 'fetch', payload: res.data})
+        }).catch((e: Error | AxiosError) => {
+            dispatch({type: 'error'})
+            console.log(e)
+        })
+
+    }, []);
+    console.log(state.data)
+    return (
+        <div className={'bg-gray-900'}>
+            <Header/>
+            <Main>asd</Main>
+        </div>
+    )
+}
+
 export default App
